fabe1b9a292ee64aa37311e001fe624aac543857
Fernando Regio <orfeu.nando@gmail.com>
2019-06-06 13:03:33 -0300

    Laboratório02 - ex4

--- Laboratório02/TypeScriptNode_Configuracao/src/index.ts
+++ Laboratório02/TypeScriptNode_Configuracao/src/index.ts
@@ -39,14 +39,43 @@
 // 3. Escreva uma função min(x,y) que devolve o menor entre dois números. Não use funções auxiliares de Math.
 // Utilize corretamente a declaração de tipos nos parâmetros e no resultado da função.
 
-function min(x: number, y: number): number{
-    if(x < y ) return x+1;
-    else return y+1;
+// function min(x: number, y: number): number{
+//     if(x < y ) return x+1;
+//     else return y+1;
+// }
+
+// console.log(min(33,5));
+
+// 4. Escreva uma função pow(x,y) que calcula o valor de 𝑥𝑦, ou seja, x elevado a potência y.
+// Assuma que os valores de x e y são números inteiros não negativos e que 𝑥0=1 para qualquer valor de x.
+// Apresente uma versão iterativa e uma versão recursiva para a função. Não utilize o operador **.
+// Utilize corretamente a declaração de tipos nos parâmetros e no resultado da função.
+
+function pow(x: number, y: number): number{
+
+    if( !(Number.isInteger(x)) || !(Number.isInteger(y))) return 0;
+    if( x <= 0 || y <= 0) return 0;
+    if( y == 0 ) return 1;
+
+    let resultado : number = x;
+
+// //versão iterativa
+// for(let i = y - 1; i > 0; i--){
+// resultado = resultado * x;
+// }
+// return resultado;
+
+//versão recursiva
+function powRecursivo(x: number, y: number): number{
+
+    if(y == 1) return x;
+    return x * powRecursivo(x, y - 1);
+}
+return powRecursivo(x,y)
 }
 
-console.log(min(33,5));
+console.log(pow(3,3))
 
-// 4. Escreva uma função pow(x,y) que calcula o valor de 𝑥𝑦, ou seja, x elevado a potência y. Assuma que os valores de x e y são números inteiros não negativos e que 𝑥0=1 para qualquer valor de x. Apresente uma versão iterativa e uma versão recursiva para a função. Não utilize o operador **. Utilize corretamente a declaração de tipos nos parâmetros e no resultado da função.
 // 5. Escreva uma função toMaiusculaPrimeira(s) que recebe uma string s (assuma qualquer string não vazia) e retorna a mesma string com a primeira letra em maiúscula. Utilize corretamente a declaração de tipos nos parâmetros e no resultado da função.
 // 6. Escreva uma função getMax(arr) que recebe um array de número inteiros e retorna o maior elemento encontrado no array. Assuma que o array não está vazio. Não utilize funções auxiliares de outros objetos disponibilizados pelo TypeScript. Utilize corretamente a declaração de tipos nos parâmetros e no resultado da função.
 // 7. Escreva uma função que, utilizando objetos Map, calcule a frequência de cada número presente em um determinado array contendo números inteiros. Utilize corretamente a declaração de tipos nos parâmetros e no resultado da função.
